aa321f63d768e7d4818796876675cd3914644e4c
Luke Loreti <luke@orgo.ai>
2018-05-09 17:38:57 -0700

    highlights a clicked bracket

--- matcher.js
+++ matcher.js
@@ -2,17 +2,50 @@
 
 'use strict';
 
-$(document).click(function(event){
-  const target = event.target
-  console.log(allBracketsMatch(target.innerText))
+$(document).click(function(event) {
+  
+	const targetText = event.target.innerText
+
+	if (!bracketPresentIn(targetText)) {
+		return
+	}
+	console.log(targetText)
+
+	if (allBracketsMatchIn(targetText)) {
+		var bracketClicked = wasBracketClicked(event)
+		if (bracketClicked) {
+			highlightClickedBracket(bracketClicked)
+		}
+	} else {
+		console.log("Brackets DON'T match!")
+		seekMatchesInParents(event)
+	}
+
 });
 
-const closingBrackets = {'}':'{', ']':'[', ')':'('}
+const closingBrackets = { 
+							'}':'{', 
+							']':'[', 
+							')':'('
+						}
+const bracketsRe = /\[|\{|\(|\]|\}|\)/
 // const openingHtmlTagRe = /\<[a-zA-Z]+(\s.+)?\>/ // Rather permissive regex, consider replacing w/ https://stackoverflow.com/a/3524392/3681279
-const re = new RegExp( /\[|\{|\(|\]|\}|\)/, 'g');
 
-function allBracketsMatch(str){
 
+var bracketPresentIn = str => bracketsRe.test(str)
+
+function inVicinity(char, event, margin) {
+    return (
+    		(event.offsetX >= (char.offsetLeft - margin)) &&
+    		(event.offsetX <= (char.offsetLeft + char.offsetWidth + margin)) &&
+    		(event.offsetY >= (char.offsetTop - margin)) &&
+    		(event.offsetY <= (char.offsetTop + char.offsetHeight + margin))  
+    	)
+}
+
+function allBracketsMatchIn(str) {
+
+	const re = new RegExp( bracketsRe, 'g');
 	var stack = [];
 	var returnArr;
 	while ((returnArr = re.exec(str)) !== null) {
@@ -30,4 +63,40 @@ function allBracketsMatch(str){
 		}
 	}
 	return stack.length === 0
+}
+
+function highlightClickedBracket(bracket) {
+	bracket.css({
+		"background-color": "#ff980099",
+    	"padding": "0px 2px",
+    	"font-weight": "bold"
+	})
+}
+
+function wasBracketClicked(event) {
+
+	// wrap all chars in spans
+	var target = $(event.target)
+	if (!target.data("hasBeenLettered")) {
+		$(event.target).lettering()
+		target.data("hasBeenLettered", true)
+	}
+
+	// Loop through chars and see if click was in vicinity of a 
+	// bracket. This could be 2D binary search
+	var chars = target.children('span[class^="char"]')
+	for (var char of chars) {
+		var v = inVicinity(char, event, 5)
+		if (bracketPresentIn(char.innerText) && inVicinity(char, event, 5)) {
+			console.log("Bracket CLICKED")
+			return $(char)
+		}
+	}
+	
+	console.log("Bracket not clicked")
+	return false
+}
+
+function seekMatchesInParents(event) {
+	return
 }
