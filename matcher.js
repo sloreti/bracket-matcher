48fc423c69afb1d1ae500980240360e0b3acbb86
Luke Loreti <luke@orgo.ai>
2018-05-10 15:14:15 -0700

    finding clicked bracket is done via Selection API

--- matcher.js
+++ matcher.js
@@ -2,31 +2,37 @@
 
 'use strict';
 
+const MAX_HEIGHT = 5; // Max number of nodes to go up the DOM checking for allBracketsMatchIn()
+
 $(document).click(function(event) {
-  
-	const targetText = event.target.innerText
 
-	if (!bracketPresentIn(targetText)) {
-		return
-	}
-	console.log(targetText)
+	var s = window.getSelection();
+    var range = s.getRangeAt(0);
+    var node = s.anchorNode;
+    var offset = s.anchorOffset - 1;
+    var clickedChar = node.textContent[offset]
+    if (bracketPresentIn(clickedChar)) {
+    	range.setStart(node, range.startOffset-1);
+    	range.deleteContents();
+    	var highlight = document.createElement('span');
+    	highlight.className = "highlighted-bracket";
+    	highlight.innerHTML = clickedChar;
+        range.insertNode(highlight);
+        range.collapse();
 
-	if (allBracketsMatchIn(targetText)) {
-		var bracketClicked = wasBracketClicked(event)
-		if (bracketClicked) {
-			highlightClickedBracket(bracketClicked)
-		}
-	} else {
-		console.log("Brackets DON'T match!")
-		seekMatchesInParents(event)
-	}
 
+    }
 });
 
 const closingBrackets = { 
 							'}':'{', 
 							']':'[', 
 							')':'('
+						}
+const startingBrackets = { 
+							'{':'}', 
+							'[':']', 
+							'(':')'
 						}
 const bracketsRe = /\[|\{|\(|\]|\}|\)/
 // const openingHtmlTagRe = /\<[a-zA-Z]+(\s.+)?\>/ // Rather permissive regex, consider replacing w/ https://stackoverflow.com/a/3524392/3681279
@@ -41,6 +47,48 @@ function inVicinity(char, event, margin) {
     		(event.offsetY >= (char.offsetTop - margin)) &&
     		(event.offsetY <= (char.offsetTop + char.offsetHeight + margin))  
     	)
+}
+
+function findMatching(bracket, str) {
+
+	var stack = [bracket.innerText];
+	var chars = target.children('span[class^="char"]')
+	var matchingBrackets = closingBrackets
+
+	if (bracket in closingBrackets) { // search backwards
+		chars = chars.reverse()
+		matchingBrackets = startingBrackets
+	}
+
+
+	for (var char of chars) {
+		var v = inVicinity(char, event, 5)
+		if (bracketPresentIn(char.innerText) && inVicinity(char, event, 5)) {
+			console.log("Bracket CLICKED")
+			return $(char)
+		}
+	}
+
+
+
+	var returnArr;
+	while ((returnArr = re.exec(str)) !== null) {
+
+		var bracket = returnArr[0]
+		if (bracket in matchingBrackets) {
+			var topOfStack = stack[stack.length-1]
+			if (topOfStack === closingBrackets[bracket]) {
+				var popped = stack.pop()
+				if (!stack) {
+					return popped
+				}
+			} else {
+				return false;
+			}
+		} else {
+			stack.push(bracket);
+		}
+	}
 }
 
 function allBracketsMatchIn(str) {
@@ -63,38 +111,6 @@ function allBracketsMatchIn(str) {
 		}
 	}
 	return stack.length === 0
-}
-
-function highlightClickedBracket(bracket) {
-	bracket.css({
-		"background-color": "#ff980099",
-    	"padding": "0px 2px",
-    	"font-weight": "bold"
-	})
-}
-
-function wasBracketClicked(event) {
-
-	// wrap all chars in spans
-	var target = $(event.target)
-	if (!target.data("hasBeenLettered")) {
-		$(event.target).lettering()
-		target.data("hasBeenLettered", true)
-	}
-
-	// Loop through chars and see if click was in vicinity of a 
-	// bracket. This could be 2D binary search
-	var chars = target.children('span[class^="char"]')
-	for (var char of chars) {
-		var v = inVicinity(char, event, 5)
-		if (bracketPresentIn(char.innerText) && inVicinity(char, event, 5)) {
-			console.log("Bracket CLICKED")
-			return $(char)
-		}
-	}
-	
-	console.log("Bracket not clicked")
-	return false
 }
 
 function seekMatchesInParents(event) {
