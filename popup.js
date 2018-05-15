de1d53a658a2c615ca79240295512faa5088a891
Luke Loreti <luke@orgo.ai>
2018-05-15 00:09:55 -0700

    spectrum js added

--- popup.js
+++ popup.js
@@ -1,21 +1,7 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright 2018 Luke Loreti. All rights reserved.
 
 'use strict';
 
-let changeColor = document.getElementById('changeColor');
-
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+$("#swab").spectrum({
+    color: "#f00"
 });
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
-};
