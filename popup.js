64908cb47b9e4980740bb7d18f1115505b7d10ac
Luke Loreti <luke@orgo.ai>
2018-05-15 17:28:58 -0700

    popup manages state correctly

--- popup.js
+++ popup.js
@@ -12,42 +12,68 @@ chrome.storage.sync.get(['enabled', 'color', 'highlightType'], function(data) {
 
 	color = data.color;
 	highlightType = data.highlightType;
-	$(".highlight-button").click(changeHighlightType)
-	$("#swab").spectrum({ color: color });
-
+	$("#swab").spectrum({ 
+		color: color,
+		clickoutFiresChange: true,
+		change: changeColor 
+	});
 	handleToggle()
 });
 
 
 function handleToggle() {
 	if ($("#enabled").is(":checked")) {
-		$("input:checked + .slider").css("background-color", color)
-		$("#" + highlightType).click()
+		recolor()
+		$(".highlight-button").css("cursor", "pointer").click(changeHighlightType)
 		$("#swab").spectrum("enable");
-		$("#swab").spectrum("set", color);
-		$("#label").text(color)
 	} else {
-		$("input + .slider").css("background-color", "white")
-		$(".highlight-button").css("cursor", "default")
-		$(".highlighted").css({	
-			"background-color": "#9e9e9e99",
-			"border": " 1px solid #fff"
-		});
-		$("#swab").spectrum("set", "9e9e9e");
+		grayOut()
+		$(".highlight-button").css("cursor", "default").off();
 		$("#swab").spectrum("disable");
-		$("#label").text("disabled");
 	}
 }
 
+function recolor() {
+	$("input:checked + .slider").css("background-color", color)
+	$("#swab").spectrum("set", color);
+	$("#label").text(color)
+	removeHighlights()
+	applyHighlight($("#"+highlightType))
+}
+
+function grayOut() {
+	$("input + .slider").css("background-color", "white")
+	$(".highlighted").css({	
+			"background-color": "#9e9e9e99",
+			"border": " 1px solid #fff"
+		});
+	$("#swab").spectrum("set", "9e9e9e");
+	$("#label").text("disabled");
+}
+
 function changeHighlightType() {
+	removeHighlights()
+	applyHighlight($(this))
+	highlightType = $(this).attr('id')
+	chrome.storage.sync.set({"highlightType" : highlightType})
+}
+
+function removeHighlights() {
 	$(".highlighted").css({
 		"background-color": "white",
 		"border" : "1px solid #e2e2e2"
+	})	
+}
 
-	})
-	$(this).children(".highlighted").css({	
+function applyHighlight(target) {
+	target.children(".highlighted").css({	
 		"background-color": color+"99", // append alpha value
 		"border" : "1px solid white"
 	})
-	// TODO: chrome.storage.sync.set()
+}
+
+function changeColor(newColor) {
+	color = newColor.toHexString()
+	chrome.storage.sync.set({"color" : color})
+	recolor()
 }
